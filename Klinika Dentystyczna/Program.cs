1c909316085798708b3f68975fc559495659a20c
Bartosz Meyer <bartoszek200702@gmail.com>
2026-02-05 18:48:04 +0100

    Walidacja pól i zapisywanie do pliku

--- Klinika Dentystyczna/Program.cs
+++ Klinika Dentystyczna/Program.cs
@@ -1,1 +1,51 @@
-﻿
+﻿using System;
+using System.IO;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string imie;
+        string nazwisko;
+        string plec;
+        
+        do
+        {
+            Console.WriteLine("Podaj Imie: ");
+            imie = Console.ReadLine();
+            
+            if (string.IsNullOrEmpty(imie))
+            {
+                Console.WriteLine("Nie podano imienia");
+
+            }
+            
+        } while(string.IsNullOrEmpty(imie));
+
+        do
+        {
+            Console.WriteLine("Podaj Nazwisko");
+            nazwisko = Console.ReadLine();
+
+            if(string.IsNullOrEmpty(nazwisko))
+            {
+                Console.WriteLine("Nie podano nazwiska");
+            }
+        } while (string.IsNullOrEmpty(nazwisko));
+
+        do
+        {
+            Console.WriteLine("Podaj płeć (Mężczyzna/Kobieta)");
+            plec = Console.ReadLine();
+
+            if(string.IsNullOrEmpty (plec))
+            {
+                Console.WriteLine("Nie podano płci");
+            }
+        } while (string.IsNullOrEmpty (plec));
+
+        string sciezka = "dane.txt";
+
+        File.AppendAllText(sciezka, imie + " " + nazwisko + " - " + plec + Environment.NewLine);
+        Console.WriteLine("Zarezerwowano");
+    }
+}
